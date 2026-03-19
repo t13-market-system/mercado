10087e7ad0d1c8375caf7960d4026a4d39f6cbce
diogofragoso <diogofragoso@yahoo.com.br>
2026-03-19 20:25:31 -0300

    refactor:troca do IP do banco de dados

--- obj/Debug/net8.0-windows/SistemaLogin.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/SistemaLogin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaLogin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dec684dcab9f4c8d5fb2c0b3f1c11da5ab34f5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaLogin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaLogin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
