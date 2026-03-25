29571aa5a37d69b5336f5f31bcab44c4af56861c
richardmodesto <richardmodesto15@gmail.com>
2026-03-25 20:31:50 -0300

    model cargos_funcionarios

---
+++ Models/Cargos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaLogin.Models
+{
+    internal class Cargos
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int SalarioFuncionario { get; set; }
+
+    }
+}
