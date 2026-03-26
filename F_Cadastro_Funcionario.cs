7e6726520aac14825a8dfc977035328361ea16a7
MuriloTeodoro <mucasemh@gmail.com>
2026-03-26 20:01:12 -0300

    Tela cadastro funcionário

---
+++ F_Cadastro_Funcionario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaLogin
+{
+    public partial class F_Cadastro_Funcionario : Form
+    {
+        public F_Cadastro_Funcionario()
+        {
+            InitializeComponent();
+        }
+    }
+}
