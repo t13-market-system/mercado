7f7cd883405743db103a8b18e35cc5950b8e1840
brayan508 <brayanrodrigues713@gmail.com>
2026-03-27 22:08:44 -0300

    novo-Dao-produto

--- Views/F_cad_produtos.cs
+++ Views/F_cad_produtos.cs
@@ -56,63 +56,15 @@ namespace SistemaLogin
 
         private void BT_cadastro_Click(object sender, EventArgs e)
         {
-            string nomeProduto = tb_nomeProduto.Text.Trim();
-            string categoria = CB_categoria.Text;
-            string fornecedor = CB_fornecedor.Text;
-            string precoProduto = TB_precoProduto.Text.Trim();
-            string codigo = TB_codigo.Text.Trim();
 
-            // 1. Validação básica
-            if (string.IsNullOrEmpty(nomeProduto) ||
-                string.IsNullOrEmpty(categoria) ||
-                string.IsNullOrEmpty(fornecedor) ||
-                string.IsNullOrEmpty(precoProduto) ||
-                string.IsNullOrEmpty(codigo))
-            {
-                MessageBox.Show("Preencha todos os campos para cadastrar!",
-                                "Validação",
-                                MessageBoxButtons.OK,
-                                MessageBoxIcon.Warning);
-                return;
-            }
-
-            try
-            {
-                // 2. Converter preço
-                decimal preco;
-                if (!decimal.TryParse(precoProduto, out preco))
-                {
-                    MessageBox.Show("Preço inválido!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-
-
-                // 4. Criar objeto Produto
-                Produto produto = new Produto
-                {
-                    Nome_produto = nomeProduto,
-                    Id_categoria = Convert.ToInt32(CB_categoria.SelectedValue),
-                    Id_fornecedor = Convert.ToInt32(CB_fornecedor.SelectedValue),
-                    Preco_produto = preco,
-                    Codigo_produto = codigo
-                };
-
-                // 5. Salvar
-                produtoDAO1 produtoDAO = new produtoDAO1();
-                produtoDAO.AdicionarProduto(produto);
-
-                MessageBox.Show("Produto cadastrado com sucesso!",
-                                "Sucesso",
-                                MessageBoxButtons.OK,
-                                MessageBoxIcon.Information);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CB_fornecedor_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
