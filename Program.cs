954c165f15b7cab3adfafb7cd35a21f5327de2cb
diogofragoso <diogofragoso@yahoo.com.br>
2026-04-02 19:37:10 -0300

    ajuste tela inicial

--- Program.cs
+++ Program.cs
@@ -12,8 +12,7 @@ namespace SistemaLogin
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new F_Relatorio_Vendas());
+            ApplicationConfiguration.Initialize();          
         }
     }
 }
