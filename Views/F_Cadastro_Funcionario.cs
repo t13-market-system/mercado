b9a62cd812991e55aacb7ed234e856ef08f50f47
Felipe.santos <felipeevdossantos@gmail.com>
2026-04-01 21:49:19 -0300

    atualizado DAOfuncionario e os models e a CRUD funcionarios

--- Views/F_Cadastro_Funcionario.cs
+++ Views/F_Cadastro_Funcionario.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SistemaLogin.DAO;
+using SistemaLogin.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -25,6 +27,97 @@ namespace SistemaLogin
         private void label10_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void txtCidade_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNome.Text.Trim())) return;
+
+            funcionarios novofuncionario = new funcionarios();
+
+            novofuncionario.Nome = txtNome.Text.Trim();
+
+            Endereco_funcionario endereco = new Endereco_funcionario();
+
+            contato_funcionario contato = new contato_funcionario();
+
+            FuncionarioDAO dao = new FuncionarioDAO();
+
+            dao.Adicionarfuncionarios(novofuncionario, endereco, contato);
+            MessageBox.Show("funcionario adicionado com sucesso");
+            txtNome.Clear();
+
+
+
+
+
+
+
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+        }
+
+        private void btn_deletar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNome.Text.Trim())) return;
+
+            funcionarios novofuncionario = new funcionarios();
+
+            novofuncionario.Nome = txtNome.Text.Trim();
+
+            Endereco_funcionario endereco = new Endereco_funcionario();
+
+            contato_funcionario contato = new contato_funcionario();
+
+            FuncionarioDAO dao = new FuncionarioDAO();
+
+            dao.ExcluirFuncionario(novofuncionario, endereco, contato);
+            MessageBox.Show("funcionario deletado com sucesso");
+            txtNome.Clear();
+
+        }
+
+        private void btn_atualizar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNome.Text.Trim())) return;
+
+            funcionarios funcionario = new funcionarios();
+
+            funcionario.Nome = txtNome.Text.Trim();
+            funcionario.email_funcionario = txtEmail.Text.Trim();
+            
+
+            Endereco_funcionario endereco = new Endereco_funcionario();
+            endereco.Pais_funcionario = txtPais.Text.Trim();
+            endereco.Estado_funcionario = txtEstado.Text.Trim();
+            endereco.Cidade_funcionario = txtEstado.Text.Trim();
+            endereco.Bairro_funcionario =txtBairro.Text.Trim(); 
+            endereco.Rua_funcionario=txtRua.Text.Trim();    
+            endereco.Numero_rua = txtNumero.Text.Trim();    
+            endereco.Cep_funcionario = txt_cep.Text.Trim();
+            endereco.Complemento_funcionario = txtComplemento.Text.Trim();  
+          
+            
+
+            contato_funcionario contato = new contato_funcionario();
+            contato.telefone_funcionario = txtTelefone.Text.Trim();
+
+            FuncionarioDAO dao = new FuncionarioDAO();
+
+            dao.AtualizarFuncionario(funcionario, endereco, contato);
+            MessageBox.Show("");
+            txtNome.Clear();
         }
     }
 }
