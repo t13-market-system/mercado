ebff596da71e48310f91821e50e8a36919c2c51d
diogofragoso <diogofragoso@yahoo.com.br>
2026-04-02 21:08:27 -0300

    Bug: Ajuste tela inicial

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace SistemaLogin
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new F_Cad_Fornecedor());
+            Application.Run(new Form1());
         }
     }
 }
